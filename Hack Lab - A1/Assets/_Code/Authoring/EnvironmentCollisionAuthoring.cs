5a974eb5ae145e10445767c95ccb029eaf5503a0
Tyler <dreamingimlatios@gmail.com>
2024-05-27 17:13:06 -0500

    Create A1 Project Skeleton
    PolygonalMind uses child collider meshes, except they import as mesh renderers rather than as colliders. There might be a way to automate this in the future. But for now, I swapped them manually. The environment seems like a good test for character controllers, given it is designed for player avatars to explore.

---
+++ Hack Lab - A1/Assets/_Code/Authoring/EnvironmentCollisionAuthoring.cs
@@ -0,0 +1,20 @@
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace A1
+{
+    public class EnvironmentCollisionAuthoring : MonoBehaviour
+    {
+        
+    }
+
+    public class EnvironmentCollisionAuthoringBaker : Baker<EnvironmentCollisionAuthoring>
+    {
+        public override void Bake(EnvironmentCollisionAuthoring authoring)
+        {
+            
+        }
+    }
+}
---
+++ Hack Lab - A1/Assets/_Code/Authoring/FirstPersonControllerAuthoring.cs
@@ -0,0 +1,20 @@
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace A1
+{
+    public class FirstPersonControllerAuthoring : MonoBehaviour
+    {
+        
+    }
+
+    public class FirstPersonControllerAuthoringBaker : Baker<FirstPersonControllerAuthoring>
+    {
+        public override void Bake(FirstPersonControllerAuthoring authoring)
+        {
+            
+        }
+    }
+}
